f757ef76f7041ac661119d211e900785fe8da3f2
Steffen Kahl <stevenpaw209@gmail.com>
2023-11-02 17:35:13 +0100

    [ADDED] Unity Project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField] private List<GameObject> Snails;
+    [SerializeField] private List<GameObject> WalkableAreas;
+
+    public static GameManager INSTANCE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
